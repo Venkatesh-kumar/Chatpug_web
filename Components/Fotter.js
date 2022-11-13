80eab0083b6f41dee179707e06e4facf29139a9e
unknown <venkateshkumar1405@gmail.com>
2022-11-13 12:48:06 +0530

    First commit

---
+++ Components/Fotter.js
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import Styles from "../styles/Fotter.module.css";
+import {FaFacebookSquare,FaTwitterSquare,FaHeart,FaInstagramSquare} from "react-icons/fa";
+import Image from "next/image";
+import logo from "../Images/logo.png"
+const Fotter = () =>{
+    return(
+        <section className="ffooter">
+            <footer className="page-footer">
+                <div className="container">
+                <div className="row">
+                    <div className="col l6 s12">
+                    <h5 >SSL SECURED PAYMENT</h5>
+                    <p className="grey-text text-lighten-4 ">Your information is secured by 256-bit SSL encryption.</p>
+                    <br></br>
+                    <p className="grey-text text-lighten-4">Follow us on</p>
+                    <div>
+                        
+                    <a href="https://www.facebook.com/snapwheels.online" target="_blank"  rel="noreferrer" className="white-text"><FaFacebookSquare size={24} round={true}></FaFacebookSquare> &nbsp;</a>
+                    <a href="https://twitter.com/ImSnapwheels" target="_blank" rel="noreferrer" className="white-text"><FaTwitterSquare size={24} round={true}></FaTwitterSquare>&nbsp;</a>
+                    <a href="https://www.instagram.com/snapwheels/" target="_blank" rel="noreferrer" className="white-text"><FaInstagramSquare size={24} round={true}></FaInstagramSquare></a>
+                    </div>
+                    </div>
+                    <div className="col l4 offset-l2 s12">
+                        <br />
+                        <ul>
+                            <li><Link href="/termsandconditions" ><p className={Styles.footerLinkText}>Terms and Condition</p></Link></li>
+                            <li><Link href="/privacypolicy"><p className={Styles.footerLinkText}>Privacy Policy</p></Link></li>
+                            <li><Link href="/CommunityGuidelines"><p className={Styles.footerLinkText}>Community Guidelines</p></Link></li>
+                        
+                        </ul>
+                        <br />
+                        <Image src={logo} alt="Snapwheels Logo" height={50} width={140}></Image>
+                    </div>
+                </div>
+                </div>
+                <div className="footer-copyright">
+                <div className="container center">
+                Made with &nbsp;<FaHeart size={16} color='#D82E2F' />&nbsp; by team Chatpug
+                </div>
+                </div>
+            </footer>
+            <style jsx>{`
+                .page-footer{
+                    background-color: #01caba;
+                }
+            `}</style>
+        </section>
+    )
+}
+
+export default Fotter
---
+++ Components/Navbar.js
@@ -0,0 +1,60 @@
+import Link from "next/link"
+import { useEffect } from "react"
+import $ from 'jquery';
+import {FaBars} from "react-icons/fa"
+import logo from "../Images/logo.png"
+import Image from "next/image";
+
+if (typeof window !== 'undefined') {
+    window.$ = $;
+    window.jQuery = $;
+    require('materialize-css');
+}
+
+export default function Navbar(){
+
+    useEffect(() => {
+        $('.sidenav').sidenav();
+    }, [])
+    return(
+        <section className="fnavbar">
+                <div className="navbar-fixed">
+                <nav className="nav-fixed">
+                    <div className="nav-wrapper">
+                       <Image src={logo} alt="Chatpug" height={50} width={140} />
+                        <a href="#" data-target="mobile-demo" className="sidenav-trigger hide-on-med-and-up"><FaBars size={24} round={true} /></a>
+                        <ul id="nav-mobile" className="right  hide-on-med-and-down ">
+                            <li ><Link href="/" >Home</Link></li>
+                            {/* <li ><Link href="/tariff" >Tariff</Link></li> */}
+                           <li ><Link href="/support">Support</Link></li>
+                           <li > <a className="btn" onClick={()=>window.open('https://play.google.com/store/apps/details?id=com.snapwheels','_blank' )}>Download App</a></li>
+                        </ul>
+                        
+                    </div>
+                    </nav>
+                </div>
+                
+                
+                    <ul className="sidenav" id="mobile-demo">
+                        <li><Link href="/">Home</Link></li>
+                        {/* <li><Link href="/tariff">Tariff</Link></li> */}
+                        <li><Link href="/support">Support</Link></li><li > <a className="btn large downloadButton" onClick={()=>window.open('https://play.google.com/store/apps/details?id=com.snapwheels','_blank' )}>Download App</a></li>
+                        
+                    </ul>
+
+                <style jsx>
+                    {`
+                        .nav-wrapper{
+                            background-color: #01caba;
+                            padding-left: 50px;
+                            padding-right: 50px;
+                        }
+
+                        .brand-logo{
+                            font-weight:bold
+                        }
+                    `}
+                </style>
+            </section>
+    )
+}
