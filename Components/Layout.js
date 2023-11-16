2a343b5ee311f811393b2947e270486e868a6da2
venkatesh kumar <venkateshkumar1405@gmail.com>
2023-11-16 09:49:30 +0000

    Modified site meta data

--- Components/Layout.js
+++ Components/Layout.js
@@ -13,22 +13,19 @@ export default function Layout({children}){
                 <meta name="theme-color" content="#01caba"/>
                 <meta name="msvalidate.01" content="F6DE7074B46F9AD4D0FA9A2775CB1232" />
                 
-                <meta property="og:title" content="Chatpug: Text and Video chat with strangers."/>
+                <meta property="og:title" content="Chatpug: Mordern Dating application for singles looking for something meaningful."/>
                 <meta property="og:type" content="website" />
                 <meta property="og:url" content="https://www.chatpug.com/" />
-                <meta property="og:image" content="https://www.snapwheels.online/_next/static/media/model.f4eb9f3d.png" />
-                <meta property="og:description" content="Meet and talk with strangers anonymously. Chatpug matches you with one of the cool people in internet for one-to-one text or video chat. " />
-                <meta property="og:image:width" content="1200px" />
-                <meta property="og:image:height" content="630px" />
+                <meta property="og:description" content="Serious about finding true love? so are we. Join our dating program and let us help you to find your destined ones. " />
                
                 <meta name="twitter:card" content="summary" />
-                <meta name="twitter:site" content="@ImSnapwheels"/>
-                <meta name="twitter:title" content="Rent bikes in Bangalore and 40+ other cities!" />
-                <meta name="twitter:description" content="Get a well maintained bike from the most trusted bike rentals in India!" />
-                <meta name="twitter:image:src" content="https://www.snapwheels.online/_next/static/media/model.f4eb9f3d.png" />
+                <meta name="twitter:site" content="@Chatpug_com"/>
+                <meta name="twitter:title" content="Mordern Dating application for singles looking for something meaningful." />
+                <meta name="twitter:description" content="Serious about finding true love? so are we. Join our dating program and let us help you to find your destined ones." />
+    
                 
-                <title>Chatpug: Text and Video chat with strangers.</title>
-                <meta name="description" content="Meet and talk with strangers anonymously. Chatpug matches you with one of the cool people in internet for one-to-one text or video chat." />
+                <title>Chatpug: Mordern Dating application for singles looking for something meaningful.</title>
+                <meta name="description" content="Serious about finding true love? so are we. Join our dating program and let us help you to find your destined ones." />
                 <meta name="keywords" content="chatpug, random, strangers, videochat, livecam, social " />
                 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=3" />
                 <meta charset="utf-8" /> 
@@ -49,8 +46,8 @@ export default function Layout({children}){
                 <link rel="dns-prefetch" href="https://d3vp2rl7047vsp.cloudfront.net" />
                 <link rel="dns-prefetch" href="https://img.youtube.com" />
 
-                <title>Chatpug: Text and Video chat with strangers! </title>
-                <meta name="description"  key="desc" content="Meet and talk with strangers anonymously. Chatpug matches you with one of the cool people in internet for one-to-one text or video chat." />
+                <title>Chatpug: Mordern Dating application for singles looking for something meaningful. </title>
+                <meta name="description"  key="desc" content="Serious about finding true love? so are we. Join our dating program and let us help you to find your destined ones." />
                 <link rel="icon" href="/favicon.ico" />
                 
                 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
