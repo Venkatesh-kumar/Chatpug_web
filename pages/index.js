5202a6d998abcc9b1eb83a534cded5d69513a2fb
venkatesh <venkateshkumar1405@gmail.com>
2024-10-03 10:29:17 +0530

    Removed how it works

--- pages/index.js
+++ pages/index.js
@@ -50,49 +50,49 @@ export default function Home() {
                     </div>
                 </div>
             </section>
-            <section>
-            <div className="whyContainer">
-            <h3 className={styles.headerText}>How it works?</h3>
-            <div className="row">
-                <div className="col s12 m6 l4 center">
-                    <div className="card-panel" style={{backgroundColor:'#142B2E',color:'white'}}>
-                        <h2 className={styles.stepText}>STEP 1</h2>
-                        <FaUserPlus size={120} color='#F4C724'></FaUserPlus>
-                        <p>Register with us as a first step to meet your amazing handpicked dates.</p>
+            {/* <section>
+                <div className="whyContainer">
+                <h3 className={styles.headerText}>How it works?</h3>
+                <div className="row">
+                    <div className="col s12 m6 l4 center">
+                        <div className="card-panel" style={{backgroundColor:'#142B2E',color:'white'}}>
+                            <h2 className={styles.stepText}>STEP 1</h2>
+                            <FaUserPlus size={120} color='#F4C724'></FaUserPlus>
+                            <p>Register with us as a first step to meet your amazing handpicked dates.</p>
+                        </div>
                     </div>
-                </div>
-                <div className="col s12 m6 l4 center">
-                    <div className="card-panel" style={{backgroundColor:'#142B2E',color:'white'}}>
-                        <h2 className={styles.stepText}>STEP 2</h2>
-                        <FcAssistant size={120} color='#53E0BC'></FcAssistant>
-                        <p>Our executive will call you will all details and assist you with all your queries.</p>
+                    <div className="col s12 m6 l4 center">
+                        <div className="card-panel" style={{backgroundColor:'#142B2E',color:'white'}}>
+                            <h2 className={styles.stepText}>STEP 2</h2>
+                            <FcAssistant size={120} color='#53E0BC'></FcAssistant>
+                            <p>Our executive will call you will all details and assist you with all your queries.</p>
+                        </div>
                     </div>
-                </div>
-                <div className="col s12 m6 l4 center">
-                    <div className="card-panel" style={{backgroundColor:'#142B2E',color:'white'}}>
-                        <h2 className={styles.stepText}>STEP 3</h2>
-                        <FcConferenceCall size={120} color='#F4C724'></FcConferenceCall>
-                        <p>Upon clearing our screening test, you are all set to join ChatPug dating program.</p>
+                    <div className="col s12 m6 l4 center">
+                        <div className="card-panel" style={{backgroundColor:'#142B2E',color:'white'}}>
+                            <h2 className={styles.stepText}>STEP 3</h2>
+                            <FcConferenceCall size={120} color='#F4C724'></FcConferenceCall>
+                            <p>Upon clearing our screening test, you are all set to join ChatPug dating program.</p>
+                        </div>
                     </div>
-                </div>
-                <div className="col s12 m6 l4 center">
-                    <div className="card-panel" style={{backgroundColor:'#142B2E',color:'white'}}>
-                        <h2 className={styles.stepText}>STEP 4</h2>
-                        <FcLike size={120} color='#F4C724'></FcLike>
-                        <p>We will find amazing date for you and schedule a beautiful dinner date. You just meet and talk.</p>
+                    <div className="col s12 m6 l4 center">
+                        <div className="card-panel" style={{backgroundColor:'#142B2E',color:'white'}}>
+                            <h2 className={styles.stepText}>STEP 4</h2>
+                            <FcLike size={120} color='#F4C724'></FcLike>
+                            <p>We will find amazing date for you and schedule a beautiful dinner date. You just meet and talk.</p>
+                        </div>
                     </div>
-                </div>
-                <div className="col s12 m6 l4 center">
-                    <div className="card-panel" style={{backgroundColor:'#142B2E',color:'white'}}>
-                        <h2 className={styles.stepText}>STEP 5</h2>
-                        <FcSurvey size={120} color='#F4C724'></FcSurvey>
-                        <p>We will collect and share the feedback about your date so that your next date will be smoother.</p>
+                    <div className="col s12 m6 l4 center">
+                        <div className="card-panel" style={{backgroundColor:'#142B2E',color:'white'}}>
+                            <h2 className={styles.stepText}>STEP 5</h2>
+                            <FcSurvey size={120} color='#F4C724'></FcSurvey>
+                            <p>We will collect and share the feedback about your date so that your next date will be smoother.</p>
+                        </div>
                     </div>
+                    
                 </div>
-                
-            </div>
-            </div>
-        </section>
+                </div>
+            </section> */}
         <section style={{backgroundColor:'#111',width:'100%',marginTop:20,paddingBottom:40,borderRadius:20}}>
                 <div className="row container">
                 <h3 className={styles.headerText} style={{marginBottom:40,marginTop:40,fontWeight:'bold', fontFamily:'Roboto'}}>ChatPug Mobile App</h3>
