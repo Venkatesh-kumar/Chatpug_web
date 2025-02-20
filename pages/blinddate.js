c7758492a96147adb230a634c409757c0e411e43
venkatesh kumar <venkateshkumar1405@gmail.com>
2025-02-20 12:01:19 +0000

    Added blind date registration form

---
+++ pages/blinddate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function blinddate() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="max-w-4xl w-full h-[600px]">
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdF-XdAy1Sxr4ClzVBfcTcye5XevpArZWYlnNIEwau0ftizcg/viewform?embedded=true" 
+        width="100%"
+        height="1663"
+          className="rounded-lg shadow-lg" allowFullScreen>Loading…</iframe>
+      </div>
+    </div>
+  );
+}
